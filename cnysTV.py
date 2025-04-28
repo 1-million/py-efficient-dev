a6c4bd68e78aa73dd928cc1704d586e6d41d6d19
qianr <593747881@qq.com>
2025-04-29 07:51:16 +0800

    优化

--- cnysTV.py
+++ cnysTV.py
@@ -1,5 +1,5 @@
 def filter_url(flow):
-    if flow.request.pretty_url.find("cnys.tv/vodplay") > -1:
+    if flow.request.pretty_url.find("cnys.tv") > -1:
         flow.response.text = flow.response.text.replace("setInterval(loop, 1);", "console.log('debug');")
     if flow.request.pretty_url.find("addons/dp/player/js/player.js") > -1:
         flow.response.headers.update({"content-type": "application/javascript; charset=utf-8"})
--- proxy.py
+++ proxy.py
@@ -18,7 +18,7 @@ def response(flow: http.HTTPFlow):
     #lcz_company.filter_url(flow)
     #rktong.filter_url(flow)
     cnysTV.filter_url(flow)
-    # print(f"请求:{flow.request.method}->{flow.request.pretty_url}")
+    print(f"请求:{flow.request.method}->{flow.request.pretty_url}")
     # print(f"响应:{flow.response.status_code}")
     # print(f"内容:{flow.response.text}")
 
@@ -26,5 +26,5 @@ def response(flow: http.HTTPFlow):
 if __name__ == "__main__":
     # 运行 Mitmproxy，并传递命令行参数
     # mitmdump(['-s', __file__, '-p', '8888', '-q'])
-    mitmdump(['-s', __file__, '-r', '127.0.0.1:8888', '-q'])
+    mitmdump(['-s', __file__, '-p','8888','--mode','upstream:http://127.0.0.1:10809', '-q'])
 
